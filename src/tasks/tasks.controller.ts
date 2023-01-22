b72eca5dce5a2a6182a056f2874413c18ecfb6d8
Patrick <pvilchez794@gmail.com>
2023-01-22 17:20:22 -0500

    Prettier formatter add and controllers with error handler

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
-import { Task } from './tasks.entity';
-import { TasksService } from './tasks.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  BadRequestException,
+  NotFoundException
+} from '@nestjs/common'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { UpdateTaskDto } from './dto/update-task.dto'
+import { Task } from './tasks.entity'
+import { TasksService } from './tasks.service'
 
 @Controller('tasks')
 export class TasksController {
@@ -10,26 +21,41 @@ export class TasksController {
 
   @Get('')
   async findAll(): Promise<Task[]> {
-    return this.tasksService.findAll();
+    return this.tasksService.findAll()
   }
 
   @Post('/add')
   async create(@Body() newTask: CreateTaskDto): Promise<Task> {
-    return this.tasksService.create(newTask);
+    try {
+      return this.tasksService.create(newTask)
+    } catch (err) {
+      throw new BadRequestException('Missing task content')
+    }
   }
 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-    return this.tasksService.findOne(id);
+    try {
+      return await this.tasksService.findOne(id)
+    } catch (err) {
+      throw new NotFoundException('Task not found')
+    }
   }
 
   @Patch(':id')
-  async update(@Param('id', ParseIntPipe) id: number, @Body() task: UpdateTaskDto) {
-    return this.tasksService.update(id, task);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() task: UpdateTaskDto
+  ) {
+    try {
+      return this.tasksService.update(id, task)
+    } catch (err) {
+      throw new BadRequestException('Missing task content')
+    }
   }
 
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.tasksService.remove(id);
+    return this.tasksService.remove(id)
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,32 +1,48 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
-import { Task } from './tasks.entity';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { UpdateTaskDto } from './dto/update-task.dto'
+import { Task } from './tasks.entity'
 
 @Injectable()
 export class TasksService {
-  constructor(@InjectRepository(Task) private tasksRepository: Repository<Task>) {}
+  constructor(
+    @InjectRepository(Task) private tasksRepository: Repository<Task>
+  ) {}
 
   create(task: CreateTaskDto) {
-    const newTask = this.tasksRepository.create(task);
-    return this.tasksRepository.save(newTask);
+    if (task.content === '') {
+      throw new Error('Empty task content')
+    }
+
+    const newTask = this.tasksRepository.create(task)
+    return this.tasksRepository.save(newTask)
   }
 
   findAll() {
-    return this.tasksRepository.find();
+    return this.tasksRepository.find()
   }
 
-  findOne(id: number) {
-    return this.tasksRepository.findOneBy({ id });
+  async findOne(id: number) {
+    const found = await this.tasksRepository.findOneBy({ id })
+
+    if (found === null) {
+      throw new Error('Task not found')
+    }
+
+    return found
   }
 
   update(id: number, task: UpdateTaskDto) {
-    return this.tasksRepository.update(id, task);
+    if (task.content === '') {
+      throw new Error('Empty task content')
+    }
+
+    return this.tasksRepository.update(id, task)
   }
 
   async remove(id: number): Promise<void> {
-    await this.tasksRepository.delete(id);
+    await this.tasksRepository.delete(id)
   }
 }
