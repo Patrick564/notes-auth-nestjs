b6af230603244195b8ba8fa8e896200149d8ef5f
Patrick <pvilchez794@gmail.com>
2023-01-25 18:41:17 -0500

    Try for sync db

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard'
       // password: '12345',
       // database: 'notes',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false
+      synchronize: true
     }),
     TasksModule,
     UsersModule,
